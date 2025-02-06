fe46664d9f58bc861b9887aead64110c82e0f1ee
Gustavo Romero <gadjrs_@hotmail.com>
2025-02-06 13:58:15 -0300

    test

--- public/index.php
+++ public/index.php
@@ -6,6 +6,9 @@ define('LARAVEL_START', microtime(true));
 
 // Determine if the application is in maintenance mode...
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+
+
+
     require $maintenance;
 }
 
